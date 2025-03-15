0f4826f15aa53ee5950d4252ad4c7e4a1e95c570
olehshedlovskyi <olehshedlovskyi@gmail.com>
2025-03-15 18:38:03 +0300

    добавленны извÐменения в файлы

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
-console.log("Скрипт для урока 2 подключен и работает!");
+// // const variable  = false;
+// //
+// // console.log(typeof variable.toString());
+//
+// const obj = {
+//     a: 'value',
+//     b: 'value',
+//     c: 'value',
+// }
+//
+// const arrey = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-document.querySelector("h1").addEventListener("click", function() {
-    alert("Вы нажали на заголовок в уроке 2!");
-});
+// const
+//
+// console.log(arrey{10})
+//
+// const a = 'a';
+//
+// let b = 'a';
+//
+// b = 'b'
+//
+// console.log(b);
+// const arrey = {1, 2, 3, 4, 5, 6, 7, 8, 9,
+// //  10}
+//
+// arrey.push(11);
+//
+// console.log(arrey.reverse());
+//
+//
+// const arrey = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+//
+// // {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+// // {1, 2, 3, 4, five, 6, 7, 8, 9, 10}
+//
+// console.log(arrey)
+
+// const arrey = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+// function getsun(num1, num2) {
+//     const sun = num1 + num2;
+//     return sun;
+// }
+//
+// const sun = getsun(arrey(0), arrey(1));
+// console.log(sun)
+
+const arrey: number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+console getsun = (num1, num2) => {}
+
+const sun =  getsun(arrey(0), arrey(1));
+console.log(sun);
+
